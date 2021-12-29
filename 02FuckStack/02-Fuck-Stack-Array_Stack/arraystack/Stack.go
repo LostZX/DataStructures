2b18965b63a0dd3102aa359d25e0a620a480b727
LostZX <760833147@qq.com>
2021-12-29 14:42:28 +0800

    又学了会

---
+++ 02FuckStack/02-Fuck-Stack-Array_Stack/arraystack/Stack.go
@@ -0,0 +1,35 @@
+package arraystack
+
+import "../array"
+
+type Stack struct {
+	array *array.Array
+}
+
+func (s *Stack) Instance() *Stack {
+	return &Stack{array: array.Instance()}
+}
+
+func (s *Stack) Pop() interface{} {
+	return s.array.RemoveLast()
+}
+
+func (s *Stack) push(item interface{}) {
+	s.array.AddLast(item)
+}
+
+func (s *Stack) peek() interface{} {
+	return s.array.Get(s.GetSize() - 1)
+}
+
+func (s *Stack) GetSize() int {
+	return s.array.GetSize()
+}
+
+func (s *Stack) GetCapacity() int {
+	return s.array.GetCapacity()
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.array.IsEmpty()
+}
